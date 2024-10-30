5694b720a2ae6c8b8ef7343456c9ff558da76900
Mihai Modi <mihai.modi@gmail.com>
2024-10-30 21:07:08 +0200

    Added loop to iterate thru all pages

--- gui.py
+++ gui.py
@@ -4,16 +4,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
-pagina_start = 'https://www.olx.ro/auto-masini-moto-ambarcatiuni/autoturisme/'
+pagina = 'https://www.olx.ro/auto-masini-moto-ambarcatiuni/autoturisme/'
 
-response = requests.get(pagina_start)
-soup = BeautifulSoup(response.text, 'html.parser')
 
-lista_anunturi = soup.find_all('div', class_='css-1apmciz')
 
-for tag_anunt in lista_anunturi:
-    hyperlink_tag= tag_anunt.find('a', class_='css-z3gu2d')
-    hyperlink = hyperlink_tag.get("href")
-    if hyperlink.startswith('/d/oferta/'):
-        link_anunt = 'https://www.olx.ro' + hyperlink
-        scrape.get_data(link_anunt)
+while True:
+    response = requests.get(pagina)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    lista_anunturi = soup.find_all('div', class_='css-1apmciz')
+
+    for tag_anunt in lista_anunturi:
+        hyperlink_tag= tag_anunt.find('a', class_='css-z3gu2d')
+        hyperlink = hyperlink_tag.get("href")
+        if hyperlink.startswith('/d/oferta/'):
+            link_anunt = 'https://www.olx.ro' + hyperlink
+            scrape.get_data(link_anunt)
+
+    lista_pagini = soup.find('ul', class_='pagination-list')
+    link_pagina_urm = lista_pagini.find('a', {'data-testid': 'pagination-forward'})
+    if link_pagina_urm:
+        pagina_urm = 'https://www.olx.ro' + link_pagina_urm.get('href')
+    else:
+        break
